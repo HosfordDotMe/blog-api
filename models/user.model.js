1a6cd1ae97478cc71ab8ce6f6c38c32c5f176e06
Eric Hosford <eric.hosford@gmail.com>
2017-07-27 19:11:26 -0400

    added setPassword and isValidPassword to the user model. Additionally, changed the user.router to setPassword when the user is created. Validated that the request has a password field.

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema ({
+const userSchema = new Schema({
     email: {
         required: true,
         type: String,
@@ -14,6 +15,19 @@ const userSchema = new Schema ({
         type: String
     }
 });
+
+userSchema.methods.setPassword = function setPassword(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto
+        .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+        .toString('hex');
+}
+userSchema.methods.isValidPassword = function isValidPassword(password) {
+    const testHash = crypto
+        .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+        .toString('hex');
+    return this.hash === testHash;
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
